5a6237f36763519276b124c5fc8f0aced2b8dcd5
Sanjay BS <56668201+sanjay-mrk@users.noreply.github.com>
2022-07-07 12:15:37 +0530

    Added My 7th July First File.
    Added My 7th July First File.

---
+++ 7july.js
@@ -0,0 +1,12 @@
+function add(){
+	const n = document.getElementById('num1').value;
+	const sum = 2 * parseInt(n);
+	console.log(sum);
+}
+
+function sum(){
+	const a = document.getElementById('num1').value;
+	const mul = 2 * parseInt(a);
+	document.getElementById('result').textContent = "Result Is : " + ""+ mul;
+	//sanjay-mrk.github.io	
+}
